8eefe7662b0cb5efb31f42afe67e95ef4166445b
Zhangwei Wu <zhangweiwu@outlook.com>
2019-01-13 17:59:03 -0800

    p336,p621,p924

---
+++ LeetCode/P336-Palindrome-Pairs.cs
@@ -0,0 +1,45 @@
+﻿namespace LeetCode.P331
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Solution
+    {
+        public IList<IList<int>> PalindromePairs(string[] words)
+        {
+            var result = new List<IList<int>>();
+
+            var len = words.Length;
+            var maxlen = 0;
+            for (var i = 0; i < len; i++) if (words[i].Length > maxlen) maxlen = words[i].Length;
+            char[] cache = new char[maxlen * 2];
+            for (var i = 0; i < len; i++)
+            {
+                var wi = words[i];
+                var li = wi.Length;
+                Array.Copy(wi.ToCharArray(), cache, li);
+                for (var j = 0; j < len; j++)
+                {
+                    if (i == j) continue;
+                    var wj = words[j];
+                    var lj = wj.Length;
+                    Array.Copy(wj.ToCharArray(), 0, cache, li, lj);
+                    var totalLength = li + lj;
+                    bool add = true;
+                    for (var k = 0; k < totalLength / 2; k++)
+                    {
+                        if (cache[k] != cache[totalLength - 1 - k])
+                        {
+                            add = false;
+                            break;
+                        }
+                    }
+
+                    if (add) result.Add(new int[] { i, j });
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode/P621-Task-Scheduler.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode.P621
+{
+    using System;
+    public class Solution
+    {
+        public int LeastInterval(char[] tasks, int n)
+        {
+            var counters = new int[26];
+            for (int i = 0, len = tasks.Length; i < len; i++) counters[tasks[i] - 'A']++;
+            Array.Sort(counters, (a, b) => b - a);
+            int max = counters[0] - 1, idle = max * n;
+            for (var i = 1; i < 26 && counters[i] != 0; i++) idle -= Math.Min(counters[i], max);
+            return idle > 0 ? idle + tasks.Length : tasks.Length;
+        }
+    }
+}
