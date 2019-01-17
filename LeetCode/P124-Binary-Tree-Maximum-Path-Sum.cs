25ac4ef327e1cbbb5ec0a10f2ecfe7a3baba9365
Zhangwei Wu <zhangweiwu@outlook.com>
2019-01-17 01:10:15 -0800

    p124

---
+++ LeetCode/P124-Binary-Tree-Maximum-Path-Sum.cs
@@ -0,0 +1,37 @@
+﻿
+namespace LeetCode
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+
+    public class Solution
+    {
+        public int MaxPathSum(TreeNode root)
+        {
+            int global = int.MinValue;
+            Max(root, ref global);
+            return global;
+        }
+
+        // key is node+left?/right?, value is node+left?+right?
+        private int Max(TreeNode root, ref int global)
+        {
+            if (root == null) return 0;
+
+            var lbranch = Max(root.left, ref global);
+            var rbranch = Max(root.right, ref global);
+            
+            var tglobal = root.val + lbranch + rbranch;
+            if (tglobal > global) global = tglobal;
+
+            var branch = root.val + (lbranch > rbranch ? lbranch : rbranch);
+            return branch < 0 ? 0 : branch;
+
+        }
+    }
+}
