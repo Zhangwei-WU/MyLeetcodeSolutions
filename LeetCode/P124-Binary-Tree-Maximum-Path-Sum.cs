84f77d5a527ed645fff3c23d87278f45ff90abc1
Zhangwei Wu <zhangweiwu@outlook.com>
2019-01-23 23:43:15 -0800

    p94

--- LeetCode/P124-Binary-Tree-Maximum-Path-Sum.cs
+++ LeetCode/P124-Binary-Tree-Maximum-Path-Sum.cs
@@ -1,5 +1,5 @@
 ﻿
-namespace LeetCode
+namespace LeetCode.P124
 {
     public class TreeNode
     {
---
+++ LeetCode/P94-Binary-Tree-Inorder-Traversal.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+
+    public class Solution
+    {
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            return Rec(root).ToArray();
+        }
+
+        private IEnumerable<int> Rec(TreeNode root)
+        {
+            if (root == null) yield break;
+
+            foreach (var n in Rec(root.left)) yield return n;
+            yield return root.val;
+            foreach (var n in Rec(root.right)) yield return n;
+        }
+    }
+}
