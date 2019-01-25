bad865dd7aa71381fbbff4b880fa20b7ffefac10
Zhangwei Wu <zhangweiwu@outlook.com>
2019-01-25 00:41:37 -0800

    p201

---
+++ LeetCode/P201-Bitwise-AND-of-Numbers-Range.cs
@@ -0,0 +1,26 @@
+﻿
+namespace LeetCode.P201
+{
+    public class Solution
+    {
+        public int RangeBitwiseAnd(int m, int n)
+        {
+            var result = m;
+            var shift = 0;
+            while (result != 0 & m < n)
+            {
+                while ((result & 1) == 0)
+                {
+                    result >>= 1;
+                    m >>= 1;
+                    n >>= 1;
+                    shift++;
+                }
+
+                result &= m++;
+            }
+
+            return (result & n) << shift;
+        }
+    }
+}
