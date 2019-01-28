d6f5db4387020f54476f67668bc16fa6cbcac80c
Zhangwei Wu <zhangweiwu@outlook.com>
2019-01-27 22:10:53 -0800

    p312,p91

---
+++ LeetCode/P312-Burst-Balloons.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode.P312
+{
+    public class Solution
+    {
+        public int MaxCoins(int[] nums)
+        {
+            var len = nums.Length;
+            if (len == 0) return 0;
+            if (len == 1) return nums[0];
+
+            var cpy = new int[len + 2];
+            for (var i = 0; i < len; i++) cpy[i + 1] = nums[i];
+            cpy[0] = 1;
+            cpy[len + 1] = 1;
+
+            var dp = new int[len + 2, len + 2];
+
+            for (var i = 1; i <= len; i++)
+            {
+                for (var left = 1; left <= len - i + 1; left++)
+                {
+                    var right = left + i - 1;
+                    for (var j = left; j <= right; j++)
+                    {
+                        dp[left, right] = System.Math.Max(dp[left, right], cpy[left - 1] * cpy[j] * cpy[right + 1] + dp[left, j - 1] + dp[j + 1, right]);
+                    }
+                }
+            }
+
+            return dp[1, len];
+        }
+    }
+}
---
+++ LeetCode/P91-Decode-Ways.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.P91
+{
+    public class Solution
+    {
+        public int NumDecodings(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return 0;
+            var c = s.ToCharArray();
+            var len = c.Length;
+            if (len == 1) return c[0] == '0' ? 0 : 1;
+
+            var n2 = c[len - 1] != '0' ? 1 : 0;
+            var n1 = (c[len - 2] != '0' ? 1 : 0) * n2 + (c[len - 2] == '1' || c[len - 2] == '2' && c[len - 1] <= '6' ? 1 : 0);
+
+            for (var i = len - 3; i >= 0; i--)
+            {
+                var n = (c[i] > '0' ? 1 : 0) * n1 + (c[i] == '1' || c[i] == '2' && c[i + 1] <= '6' ? 1 : 0) * n2;
+                n2 = n1;
+                n1 = n;
+            }
+
+            return n1;
+        }
+    }
+}
