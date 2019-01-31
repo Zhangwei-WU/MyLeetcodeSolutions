5b17add2a5f75a5e6b5d7ca41aae0847b576acd8
Zhangwei Wu <zhangweiwu@outlook.com>
2019-01-30 23:00:24 -0800

    p27,p35

---
+++ LeetCode/P27-Remove-Element.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.P27
+{
+    public class Solution
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            var len = nums.Length;
+            var rlen = 0;
+            for (var i = 0; i < len; i++)
+            {
+                var n = nums[i];
+                if(val != n) nums[rlen++] = n;
+            }
+
+            return rlen;
+        }
+    }
+}
---
+++ LeetCode/P35-Search-Insert-Position.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCode.P35
+{
+    public class Solution
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            var pos = System.Array.BinarySearch(nums, target);
+            return pos < 0 ? ~pos : pos;
+        }
+    }
+}
