37d1daf2a2ae01178a598347b91312b17179dbf6
Zhangwei Wu <zhangweiwu@outlook.com>
2019-02-11 17:55:02 -0800

    p136

---
+++ LeetCode/P136-Single-Number.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace LeetCode
+{
+    public class Solution
+    {
+        public int SingleNumber(int[] nums)
+        {
+            var result = 0;
+            for(int i=0,l = nums.Length;i<l;++i) result ^= nums[i];
+            return result;
+        }
+    }
+}
