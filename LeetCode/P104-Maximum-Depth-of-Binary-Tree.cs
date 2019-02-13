5038704ed3076b36fc22c0cd7b51e66bb2a88a25
Zhangwei Wu <zhangweiwu@outlook.com>
2019-02-12 16:46:40 -0800

    'p104'

---
+++ LeetCode/P104-Maximum-Depth-of-Binary-Tree.cs
@@ -0,0 +1,14 @@
+﻿
+using LeetCode.Generics;
+
+namespace LeetCode.P104
+{
+    public class Solution
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null) return 0;
+            return System.Math.Max(MaxDepth(root.left), MaxDepth(root.right)) + 1;
+        }
+    }
+}
