738b2d9e7061a460d0bccf9f5f19a2349de67912
Zhangwei Wu <zhangweiwu@outlook.com>
2019-02-14 15:44:47 -0800

    136 add namespace

--- LeetCode/P136-Single-Number.cs
+++ LeetCode/P136-Single-Number.cs
@@ -1,13 +1,13 @@
 ﻿
 
-namespace LeetCode
+namespace LeetCode.P136
 {
     public class Solution
     {
         public int SingleNumber(int[] nums)
         {
             var result = 0;
-            for(int i=0,l = nums.Length;i<l;++i) result ^= nums[i];
+            for (int i = 0, l = nums.Length; i < l; ++i) result ^= nums[i];
             return result;
         }
     }
