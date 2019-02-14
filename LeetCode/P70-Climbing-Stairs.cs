adbc4407fadac505639135118664f8a663fa59a4
Zhangwei Wu <zhangweiwu@outlook.com>
2019-02-13 22:48:18 -0800

    p70

---
+++ LeetCode/P70-Climbing-Stairs.cs
@@ -0,0 +1,22 @@
+﻿
+namespace LeetCode.P70
+{
+    public class Solution
+    {
+        public int ClimbStairs(int n)
+        {
+            if (n == 0 || n == 1) return 1;
+
+            var v1 = 1;
+            var v2 = 1;
+            for (var i = 2; i <= n; i++)
+            {
+                var v3 = v1 + v2;
+                v1 = v2;
+                v2 = v3;
+            }
+
+            return v2;
+        }
+    }
+}
