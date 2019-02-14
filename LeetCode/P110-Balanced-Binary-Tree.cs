4e4ddd57fa3bb85a5d3f22f1df3e6a2f7c9e2938
Zhangwei Wu <zhangweiwu@outlook.com>
2019-02-14 15:41:59 -0800

    p110

---
+++ LeetCode/P110-Balanced-Binary-Tree.cs
@@ -0,0 +1,23 @@
+﻿
+using LeetCode.Generics;
+
+namespace LeetCode.P110
+{
+    public class Solution
+    {
+        public bool IsBalanced(TreeNode root)
+        {
+            int depth = 0;
+            return IsBalanced(root, out depth);
+        }
+
+        private bool IsBalanced(TreeNode root, out int depth)
+        {
+            depth = 0;
+            return root == null
+                || (IsBalanced(root.left, out int dl)
+                && IsBalanced(root.right, out int dr)
+                && (depth = System.Math.Max(dl, dr) + 1) <= System.Math.Min(dl, dr) + 2);
+        }
+    }
+}
