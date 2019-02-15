512e063c7518dd758b7e8a9daf7ec60f5704aef7
Zhangwei Wu <zhangweiwu@outlook.com>
2019-02-14 17:29:36 -0800

    p153

---
+++ LeetCode/P153-Find-Minimum-in-Rotated-Sorted-Array.cs
@@ -0,0 +1,20 @@
+﻿
+namespace LeetCode.P153
+{
+    public class Solution
+    {
+        public int FindMin(int[] nums)
+        {
+            int l = 0, h = nums.Length - 1;
+            if (h == 0 || nums[h] > nums[l]) return nums[l];
+            while (l < h)
+            {
+                var m = l + (h - l) / 2;
+                if (nums[m] > nums[h]) l = m + 1;
+                else h = m;
+            }
+
+            return nums[h];
+        }
+    }
+}
