735fe2fb6d9a7fd86263703536532db07d855e58
Zhangwei Wu <zhangweiwu@outlook.com>
2019-02-15 11:36:34 -0800

    p28

---
+++ LeetCode/P28-Implement-strStr.cs
@@ -0,0 +1,30 @@
+﻿
+namespace LeetCode.P28
+{
+    public class Solution
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            if (string.IsNullOrEmpty(needle)) return 0;
+            if (needle.Length > haystack.Length) return -1;
+
+            var hs = haystack.ToCharArray();
+            var ns = needle.ToCharArray();
+
+            var hl = hs.Length;
+            var nl = ns.Length;
+
+            for (int i = 0, j; i <= hl - nl; ++i)
+            {
+                for (j = 0; j < nl; ++j)
+                {
+                    if (hs[i + j] != ns[j]) break;
+                }
+
+                if (j == nl) return i;
+            }
+
+            return -1;
+        }
+    }
+}
