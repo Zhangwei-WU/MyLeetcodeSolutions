61b9f79a15c1ac641d802a7a69617a74a434c951
Zhangwei Wu <zhangweiwu@outlook.com>
2019-02-14 16:03:23 -0800

    p114

---
+++ LeetCode/P114-Flatten-Binary-Tree-to-Linked-List.cs
@@ -0,0 +1,38 @@
+﻿using LeetCode.Generics;
+
+namespace LeetCode
+{
+    public class Solution
+    {
+        public void Flatten(TreeNode root)
+        {
+            Flatten2(root);
+        }
+
+        /// <summary>
+        /// return last node of current tree
+        /// </summary>
+        private TreeNode Flatten2(TreeNode root)
+        {
+            if (root == null) return null;
+            var left = Flatten2(root.left);
+            var right = Flatten2(root.right);
+
+            if (left == null && right == null) return root;
+            else if (left == null) return right;
+            else if (right == null)
+            {
+                root.right = root.left;
+                root.left = null;
+                return left;
+            }
+            else
+            {
+                left.right = root.right;
+                root.right = root.left;
+                root.left = null;
+                return right;
+            }
+        }
+    }
+}
