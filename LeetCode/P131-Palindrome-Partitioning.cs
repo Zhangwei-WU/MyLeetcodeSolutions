30cff404821552b1593d07a97ab26925c2675aac
Zhangwei Wu <zhangweiwu@outlook.com>
2019-05-17 21:47:09 -0700

    update p131

--- LeetCode/P131-Palindrome-Partitioning.cs
+++ LeetCode/P131-Palindrome-Partitioning.cs
@@ -10,41 +10,32 @@ namespace LeetCode.P131
             var chs = s.ToCharArray();
             var l = chs.Length;
             var dp = new int[l, l];
-
-            for (var i = 0; i < l; ++i)
-            {
-                for (var j = i; j < l; ++j)
-                {
-                    Fill(chs, i, j, dp);
-                }
-            }
-
+            
             return new List<IList<string>>(Dfs(chs, l - 1, dp));
         }
 
         private int Fill(char[] chs, int i, int j, int[,] dp)
         {
+            if (i >= j) return 1;
             if (dp[i, j] != 0) return dp[i, j];
-            if (i > j) return 1;
-            if (i == j) return dp[i, j] = 1;
             if (chs[i] == chs[j]) return dp[i, j] = Fill(chs, i + 1, j - 1, dp);
             return dp[i, j] = -1;
         }
 
-        IEnumerable<IList<string>> Dfs(char[] chs, int i, int[,] dp)
+        IEnumerable<IList<string>> Dfs(char[] chs, int j, int[,] dp)
         {
-            if (i == -1)
+            if (j == -1)
             {
                 yield return new List<string>();
                 yield break;
             }
 
-            for (var j = i; j >= 0; --j)
+            for (var i = j; i >= 0; --i)
             {
-                if (dp[j, i] == 1)
+                if (Fill(chs, i, j, dp) == 1)
                 {
-                    var str = new string(chs, j, i - j + 1);
-                    foreach (var result in Dfs(chs, j - 1, dp))
+                    var str = new string(chs, i, j - i + 1);
+                    foreach (var result in Dfs(chs, i - 1, dp))
                     {
                         result.Add(str);
                         yield return result;
