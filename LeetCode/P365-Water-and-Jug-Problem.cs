06863bfa4482f8b29e60c845a55dbfa3c079ac48
Zhangwei Wu <zhangweiwu@outlook.com>
2020-02-15 16:09:02 -0800

    p365

---
+++ LeetCode/P365-Water-and-Jug-Problem.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.P365
+{
+
+    public class Solution
+    {
+        public bool CanMeasureWater(int x, int y, int z)
+        {
+            if (z == 0) return true;
+            if (x + y < z) return false;
+            if (x == 0) return y == z;
+            if (y == 0) return x == z;
+            var gcd = Gcd(x, y);
+            return z % gcd == 0;
+        }
+
+        private int Gcd(int x, int y)
+        {
+            var cp = x - y;
+            if (cp == 0) return x;
+            else if (cp < 0) return Gcd(x, -cp);
+            else return Gcd(cp, y);
+        }
+    }
+}
