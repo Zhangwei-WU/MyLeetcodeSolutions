a00ff5d9d6bdb40f9db7e0f0a21e9bccfe564b0a
Zhangwei Wu <zhangweiwu@outlook.com>
2020-02-17 00:43:03 -0800

    p442

--- LeetCode.Test/Tests4.cs
+++ LeetCode.Test/Tests4.cs
@@ -15,5 +15,13 @@
             Assert.IsFalse(sln.CheckPossibility(new int[] { 4, 2, 1 }));
             Assert.IsFalse(sln.CheckPossibility(new int[] { 3, 4, 2, 3 }));
         }
+
+        [TestMethod]
+        public void TestP442()
+        {
+            var sln = new P442.Solution();
+            CollectionAssert.AreEquivalent(new int[] { 2, 3 }, sln.FindDuplicates(new int[] { 4, 3, 2, 7, 8, 2, 3, 1 }).ToArray());
+            CollectionAssert.AreEquivalent(new int[] { 1, 10 }, sln.FindDuplicates(new int[] { 10, 2, 5, 10, 9, 1, 1, 4, 3, 7 }).ToArray());
+        }
     }
 }
---
+++ LeetCode/P442-Find-All-Duplicates-in-an-Array.cs
@@ -0,0 +1,32 @@
+﻿
+namespace LeetCode.P442
+{
+    using System.Collections.Generic;
+
+    public class Solution
+    {
+        public IList<int> FindDuplicates(int[] nums)
+        {
+            var result = new List<int>(nums.Length / 2);
+            for (var i = 0; i < nums.Length; ++i)
+            {
+                for (int j = i, n = 0; nums[j] != j + 1; j = n - 1)
+                {
+                    var t = nums[j];
+                    nums[j] = n;
+                    n = t;
+
+                    if (n == 0) break;
+
+                    if (nums[n - 1] == n)
+                    {
+                        result.Add(n);
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
