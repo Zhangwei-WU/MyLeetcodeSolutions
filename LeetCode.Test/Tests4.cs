c2fc376b9c8ca3f029c79c93e910a71953c84ef9
Zhangwei Wu <zhangweiwu@outlook.com>
2020-02-16 23:58:40 -0800

    p665

---
+++ LeetCode.Test/Tests4.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Test
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Collections.Generic;
+    using System.Linq;
+    using LeetCode.Generics;
+
+    public partial class Tests
+    {
+        [TestMethod]
+        public void TestP665()
+        {
+            var sln = new P665.Solution();
+            Assert.IsTrue(sln.CheckPossibility(new int[] { 4, 2, 3 }));
+            Assert.IsFalse(sln.CheckPossibility(new int[] { 4, 2, 1 }));
+            Assert.IsFalse(sln.CheckPossibility(new int[] { 3, 4, 2, 3 }));
+        }
+    }
+}
---
+++ LeetCode/P665-Non-decreasing-Array.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.P665
+{
+    public class Solution
+    {
+        public bool CheckPossibility(int[] nums)
+        {
+            var cnt = 0;
+            for (var i = 0; i < nums.Length - 1; ++i)
+            {
+                if (nums[i] > nums[i + 1])
+                {
+                    if (i + 2 == nums.Length || nums[i + 2] >= nums[i]) nums[i + 1] = nums[i];
+                    else if (i == 0 || nums[i + 1] >= nums[i - 1]) nums[i] = nums[i + 1];
+                    else return false;
+
+                    if (++cnt > 1) return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
